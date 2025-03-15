d417321bd463c347d5ce86ab39341430ec146140
Kak <kak@localhost>
2025-03-15 14:13:20 +0100

    Improved sheetfile format

--- TbmToSun/InstructionSheet.cs
+++ TbmToSun/InstructionSheet.cs
@@ -10,14 +10,17 @@ namespace TbmToSun
         public InstructionSheet(string file)
         {
             var cmds = File.ReadAllLines(file);
+            var inputPath = "";
             for (var i = 0; i < cmds.Length; i++)
             {
                 var cmd = cmds[i].Trim();
                 if (!cmd.StartsWith(';'))
                 {
-                    cmd = cmd.Split(';')[0];
+                    cmd = cmd.Split(';')[0]; // Remove everything to the right of the first ;
                     if (cmd.StartsWith("OutputPath="))
                         OutputPath = cmd.Split('=')[1];
+                    else if (cmd.StartsWith("InputPath="))
+                        inputPath = cmd.Split('=')[1];
                     else if (cmd.StartsWith("SplitOn="))
                         SplitOn = int.Parse(cmd.Split('=')[1]);
                     else
@@ -25,28 +28,50 @@ namespace TbmToSun
                         var row = cmds[i].Split(",").Select(x => x.Trim()).ToArray();
                         if (row.Length > 1)
                         {
-                            Console.WriteLine($"-> {row[0]}");
-                            using var input = new FileStream(row[0], FileMode.Open, FileAccess.Read);
-                            Rows.Add(new InstructionSong
+                            var fullPath = Path.Combine(inputPath, row[0]);
+                            Console.WriteLine($"-> {fullPath}");
+                            if (Directory.Exists(fullPath))
                             {
-                                Path = row[0],
-                                Module = new TbmModule(input),
-                                Kind = row[1].ToUpperInvariant() switch
-                                {
-                                    "B" => SongKind.BGM,
-                                    "S" => SongKind.SFX,
-                                    "P" => SongKind.Pause,
-                                    "U" => SongKind.Unpause,
-                                    _ => 0,
-                                },
-                                Title = row.Length > 2 ? row[2] : Path.GetFileNameWithoutExtension(row[0]),
-                            });
+                                var found = Directory.GetFiles("C:\\soundtemp", "*.tbm", SearchOption.AllDirectories);
+                                foreach (var x in found)
+                                    AddFile(x, row);
+                            }
+                            else if (!File.Exists(fullPath))
+                                Console.WriteLine($"!! This file doesn't exist.");
+                            else
+                                AddFile(fullPath, row);
                         }
                     }
                 }
             }
             if (OutputPath == null || Rows.Count == 0)
                 throw new FormatException("Malformed output file");
+        }
+
+        private void AddFile(string fullPath, string[] row)
+        {
+            try
+            {
+                using var input = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+                Rows.Add(new InstructionSong
+                {
+                    Path = fullPath,
+                    Module = new TbmModule(input),
+                    Kind = row[1].ToUpperInvariant() switch
+                    {
+                        "B" => SongKind.BGM,
+                        "S" => SongKind.SFX,
+                        "P" => SongKind.Pause,
+                        "U" => SongKind.Unpause,
+                        _ => 0,
+                    },
+                    Title = row.Length > 2 ? row[2] : Path.GetFileNameWithoutExtension(row[0]),
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
     public class InstructionSong
